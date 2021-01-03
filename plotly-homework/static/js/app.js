3d8fe16846cc983857aa4cd457eacc3cb30bdde0
Jackie Yi <jackieyi@jackies-mbp.home>
2021-01-03 15:02:07 -0800

    final push

--- plotly-homework/static/js/app.js
+++ plotly-homework/static/js/app.js
@@ -1,126 +1,182 @@
-var samples = "";
+function init() {
+	const data = d3.json("../data/samples.json").then( (data) => {
+		const sample_data = data.samples;
+        const sample_ids = [];
+        const metadata_data = data.metadata;
+        
+		for(const sample of sample_data) {
+			sample_ids.push(sample.id)
+		}
+		d3.select("#selDataset").selectAll('myOptions').data(sample_ids).enter().append('option').text(function(d) {return d; }).attr("value", function(d) {return d; });
+		var selected_id = 944;
 
-d3.json("../data/samples.json").then((data) => {
-    samples = data;
-    // dropdown menu
-    d3.select("#selDataset").selectAll("option")
-        .data(data.metadata)
-        .enter()
-        .append("option")
-        .classed(data.metadata.id, true)
-        .html(function(d) {
-            return d.id;
-    });
+		var selected_sample = find_sample_by_id(sample_data, selected_id);
 
-    // bar graph
-    var patientID = data.names[0];
-    var index = data.samples.map((e) => e.id).indexOf(patientID);
+		var y_labels = [];
+		for(const label of selected_sample.otu_ids.slice(0,10).reverse()){
+			y_labels.push(`OTU ${label}`);
+		}
+		var plot_data = [{
+			type: 'bar',
+			x: selected_sample.sample_values.slice(0,10).reverse(),
+			y: y_labels,
+			text: selected_sample.otu_labels.slice(0,10).reverse(),
+			orientation: 'h'
+		}];
+		
+		
+		var layout = {
+			title: `Top 10 OTU for Patient ID: ${selected_id}`,
+			yaxis: {
+				type: "category"
+			}
+		}
+		var CHART = d3.selectAll("#plot1").node();
+        Plotly.newPlot(CHART, plot_data, layout);
+        
+        var CHART = d3.selectAll("#plot1").node();
+        var CHART2 = d3.selectAll("#plot2").node();
+        Plotly.newPlot(CHART, plot_data, layout);
+        var bubbleX = selected_sample.otu_ids;
+        var bubbleY = selected_sample.sample_values;
+        var sizes = selected_sample.sample_values;
+        var colors = selected_sample.otu_ids;
+        var bubbleText = selected_sample.otu_labels;
+        
+        var trace2 = {
+            x: bubbleX,
+            y: bubbleY,
+            mode: 'markers',
+            text: bubbleText,
+            marker: {
+                size: sizes,
+                color: colors
+            }
+        };
 
-    var x = [];
-    var y = [];
-    var text = [];
+        var data2 = [trace2];
 
-    for (var i = 0; i < 10; i++) {
-        x.push(data.samples[index].sample_values[i]);
-        y.push(data.samples[index].otu_ids[i]);
-        text.push(data.samples[index].otu_labels[i]);
-    };
+        var layout2 = {
+            title: `Bubble Chart of OTUs found in Patient ${selected_id}`,
+            xaxis: {title: "OTU ID"},
+            yaxis: {title: "Sample Values"}
+        };
+        
+        Plotly.newPlot(CHART2, data2, layout2);
+        var patientInfo = find_sample_by_id(metadata_data, selected_id)
+        var infoPanel = d3.select("#sample-metadata").node();
+        var info = patientInfo;
+        
+        
 
-    var trace1 = {
-        x: x.reverse(),
-        y: y.reverse().map((id) => `OTU ${id}`),
-        type: 'bar',
-        orientation: 'h',
-        text: text.reverse()
-    };
+        infoPanel.textContent = ""
 
-    var woop = [trace1];
-    var layout = {
-        title: `Top Ten OTUs found in Patient ${patientID}`,
-        xaxis: {title: "Sample Values"},
-        yaxis: {title: "OTU IDs"}
-    };
+        var infoString = ""
+        Object.entries(patientInfo).forEach( ([key,value]) => {
+            infoString += `${key}: ${value} \n`;
+        });
 
-    Plotly.newPlot("bar", woop, layout);
+        infoPanel.append(infoString);
 
-    // initialized bubble chart
-    var bubbleX = data.samples[index].otu_ids;
-    var bubbleY = data.samples[index].sample_values;
-    var sizes = data.samples[index].sample_values;
-    var colors = data.samples[index].otu_ids;
-    var bubbleText = data.samples[index].otu_labels;
+        
+
+
+ 
+
+	});
+	
+
+}
+
+function find_sample_by_id(samples, id){
+	for(const sample of samples){
+		if(sample.id == id){
+			return sample
+		}
+	}
+}
+
+d3.selectAll("body").on("change",updatePlotly); 
+
+function updatePlotly() {
+	const data = d3.json("samples.json").then( (data) => {
+        const sample_data = data.samples;
+        const metadata_data = data.metadata;
+
+
+		var selected_id = d3.select("#selDataset").node().value;
+
+		var selected_sample = find_sample_by_id(sample_data, selected_id);
+
+		var y_labels = [];
+		for(const label of selected_sample.otu_ids.slice(0,10).reverse()){
+			y_labels.push(`OTU ${label}`);
+		}
+		var plot_data = [{
+			type: 'bar',
+			x: selected_sample.sample_values.slice(0,10).reverse(),
+			y: y_labels,
+			text: selected_sample.otu_labels.slice(0,10).reverse(),
+			orientation: 'h'
+		}];
+		
+		var layout = {
+			title: `Top 10 OTU for Patient ID: ${selected_id}`,
+			yaxis: {
+				type: "category"
+			}
+		}
+        var CHART = d3.selectAll("#plot1").node();
+        var CHART2 = d3.selectAll("#plot2").node();
+        Plotly.newPlot(CHART, plot_data, layout);
+        var bubbleX = selected_sample.otu_ids;
+        var bubbleY = selected_sample.sample_values;
+        var sizes = selected_sample.sample_values;
+        var colors = selected_sample.otu_ids;
+        var bubbleText = selected_sample.otu_labels;
+        
+        var trace2 = {
+            x: bubbleX,
+            y: bubbleY,
+            mode: 'markers',
+            text: bubbleText,
+            marker: {
+                size: sizes,
+                color: colors
+            }
+        };
+
+        var data2 = [trace2];
+
+        var layout2 = {
+            title: `Bubble Chart of OTUs found in Patient ${selected_id}`,
+            xaxis: {title: "OTU ID"},
+            yaxis: {title: "Sample Values"}
+        };
+        
+        Plotly.newPlot(CHART2, data2, layout2);
+        var patientInfo = find_sample_by_id(metadata_data, selected_id)
+        var infoPanel = d3.select("#sample-metadata").node();
+        var info = patientInfo;
+        
+        
+
+        infoPanel.textContent = ""
+
+        var infoString = ""
+        Object.entries(patientInfo).forEach( ([key,value]) => {
+            infoString += `${key}: ${value} \n`;
+        });
+
+        infoPanel.append(infoString);
+
+        
+
+
+    });}
+        
     
-    var trace2 = {
-        x: bubbleX,
-        y: bubbleY,
-        mode: 'markers',
-        text: bubbleText,
-        marker: {
-            size: sizes,
-            color: colors
-        }
-    };
-
-    var data2 = [trace2];
-
-    var layout2 = {
-        title: `Bubble Chart of OTUs found in Patient ${patientID}`,
-        xaxis: {title: "OTU ID"},
-        yaxis: {title: "Sample Values"}
-    };
+  
     
-    Plotly.newPlot("bubble", data2, layout2);
+init();
 
-    // display individual's demographic information
-    var informationPanel = d3.select("#sample-metadata");
-    var information = data.metadata[index];
-    
-    Object.entries(information).forEach(([key, value]) => {
-        informationPanel
-            .append("p")
-            .html(`${key}: ${value}`)
-    });
-
-});
-
-
-function optionChanged(patientID) {
-    console.log(patientID);
-    
-    
-    var patientID = patientID;
-    var index = samples.samples.map((e) => e.id).indexOf(patientID);
-
-    var informationPanel = d3.select("#sample-metadata");
-    var information = samples.metadata[index];
-    
-    var numOTUs = samples.samples[index].otu_ids.length;
-
-    var x = [];
-    var y = [];
-    var text = [];
-    var bubbleX = samples.samples[index].otu_ids;
-    var bubbleY = samples.samples[index].sample_values;
-    var updateMarker = {marker: 
-        {size: samples.samples[index].sample_values, 
-        color: samples.samples[index].otu_ids}};
-    var bubbleText = samples.samples[index].otu_labels;
-
-    x = x.reverse();
-    y = y.reverse().map((id) => `OTU ${id}`);
-    text = text.reverse();
-
-    var updateBar = {
-        title: `Top Ten OTUs found in Patient ${patientID}`
-    }
-    var updateBubble = {
-        title: `Bubble Chart of OTUs found in Patient ${patientID}`
-    }
-
-    d3.selectAll("p").remove();
-    Object.entries(information).forEach(([key, value]) => {
-        informationPanel
-            .append("p")
-            .text(`${key}: ${value}`)
-    });
-};
